3b29444d3a290ab815e01dbcc44ddaa91c5d2d8d
Ficoto <438500829@qq.com>
2022-10-20 18:09:04 +0800

    细节调整

--- generator/args.go
+++ generator/args.go
@@ -8,14 +8,14 @@ import (
 type options struct {
 	dataSourceName string
 	tableNames     []string
-	tag            string
+	tags           []string
 	forceCases     []string
 }
 
 func printUsageAndExit(exampleDataSourceName string) {
 	cmd := os.Args[0]
 	_, _ = fmt.Fprintf(os.Stderr, `Usage:
-	%s -o outpath -dbc databaseConnection [-t table1,table2,...] [-tag gorm] [-forcecases ID,IDs,HTML]
+	%s -o outpath -dbc databaseConnection [-t table1,table2,...] [-tag gorm,json,pg] [-forcecases ID,IDs,HTML]
 Example:
 	%s "%s"
 `, cmd, cmd, fmt.Sprintf("-o ./ -d %s", exampleDataSourceName))
